55f2ac5fc5e4d74ede89da5cea260c875c053e27
kostyayurchenko84 <kostyayurchenko84@yandex.ru>
2025-10-28 11:26:10 +0300

    feat:ready carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function dateSample( sampleActivity ) {
+  if(typeof sampleActivity !== 'string' || sampleActivity === undefined) {
+    return false;
+  }
+ 
+  const activity = parseFloat(sampleActivity);
+  if (isNaN(activity) || activity <= 0 || activity > MODERN_ACTIVITY) {
+    return false;
+  }
+
+  return Math.ceil((HALF_LIFE_PERIOD / 0.693) * Math.log(MODERN_ACTIVITY / sampleActivity));
 }
 
 module.exports = {
