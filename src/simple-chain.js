04656ff65c2f36d4d4cf7711b82980b5954ab8c9
kostyayurchenko84 <kostyayurchenko84@yandex.ru>
2025-11-05 15:45:40 +0300

    feat:add simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -6,25 +6,36 @@ const { NotImplementedError } = require('../lib');
  *
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  addLink( value ) {
+    let linkValue;
+    if(value === undefined) {
+      linkValue = '(  )';
+    }
+    else {
+      linkValue = `( ${value} )`;
+    }
+    this.chain.push(linkValue);
+    return this;
   },
-  removeLink(/* position */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  removeLink( position ) {
+    if(typeof position !== number || isNaN(position) || !Number.isInteger(position) || position < 1 || position > this.chain.length) {  
+      this.chain = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    return this.chain.splice(position - 1, 1);
   },
   reverseChain() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    this.chain.join('~~');
+    this.chain = [];
+    return res;
   },
 };
 
